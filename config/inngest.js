7753bb9cdabe067001e6e0e39a3ff745455fdcb5
HeryTal <jocelynraherinirina9@gmail.com>
2026-01-26 22:47:21 +0100

    Désactive middleware temporairement pour debug

--- config/inngest.js
+++ config/inngest.js
@@ -8,7 +8,7 @@ export const inngest = new Inngest({ id: "e-commerce" });
 
 //Inngest function to save user data to a database
 
-export const synUserCreation = inngest.createFunction (
+export const syncUserCreation = inngest.createFunction (
     { id:'sync-user-form-clerk'},
     { event:'clerk/user.created'},
 
@@ -16,9 +16,9 @@ export const synUserCreation = inngest.createFunction (
         const {id,first_name, last_name,email_addresses, image_url } = event.data;
         const userData = {
             _id:id,
-            email: email_addresses[0],email_address,
-            name:first_name + '' + last_name,
-            imageUrl:image_url
+            email: email_addresses[0].email_address,
+            name: first_name + ' ' + last_name,
+            imageUrl: image_url
         }
         await connectDB ()
         await User.create(userData)
