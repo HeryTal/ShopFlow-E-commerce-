82afef7c1da8874abc9cae77e96c2e4c5cbae3cf
HeryTal <jocelynraherinirina9@gmail.com>
2026-02-21 20:33:08 +0100

    route get

---
+++ app/api/cart/get/route.js
@@ -0,0 +1,26 @@
+import connectDB from "@/config/db";
+import User from "@/models/User";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+export async function GET(request) {
+    
+    try {
+        const {userId} = getAuth();
+        await connectDB();
+        const user = await User.findById(userId);
+        const cartItems = user?.cartItems || {};
+        return NextResponse.json({ success: true, cartItems });
+
+
+
+}
+    catch (error) {
+        console.error("Error fetching cart items:", error);
+        return NextResponse.json({ success: false, message: "Failed to fetch cart items" }, { status: 500 });
+
+    }
+
+
+}
