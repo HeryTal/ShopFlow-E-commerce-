2b7c43d70e093103fd4d7333dfc8d3840149c14a
HeryTal <jocelynraherinirina9@gmail.com>
2026-06-19 07:54:39 +0200

    correction de l'API Clerk

--- app/api/cart/get/route.js
+++ app/api/cart/get/route.js
@@ -1,22 +1,25 @@
 import connectDB from "@/config/db";
 import User from "@/models/User";
-import { getAuth } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
 export async function GET(request) {
-    
     try {
-        const {userId} = getAuth();
+        const { userId } = await auth();
+
+        if (!userId) {
+            return NextResponse.json({ success: false, message: "Unauthorized" }, { status: 401 });
+        }
+
         await connectDB();
-        const user = await User.findById(userId);
+        const user = await User.findOne({ clerkId: userId });
         const cartItems = user?.cartItems || {};
+
         return NextResponse.json({ success: true, cartItems });
 
-}
-    catch (error) {
+    } catch (error) {
         console.error("Error fetching cart item:", error);
         return NextResponse.json({ success: false, message: "Failed to fetch cart item" }, { status: 500 });
-
     }
 }
